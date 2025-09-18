656834248864b16c6b730f4a294eb3eeb3b6be09
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-09-18 15:01:33 -0400

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -3,14 +3,15 @@ import cors from "cors";
 import jobsRoutes from "./Routes/jobsRoutes.js";
 import usersRoutes from "./Routes/usersRoutes.js";
 
+// Middleware
 const app = express();
+app.use(express.json()); // parse JSON request bodies
+app.use(cors()); //allows requests from the frontend
+
 app.use("/jobs", jobsRoutes);
 app.use("/users", usersRoutes);
 
-// ???????????????????? Is this the right place for the code???
-// Middleware
-app.use(cors()); //allows requests from the frontend
-app.use(express.json()); // parse JSON request bodies
+app.get("/", (req, res) => res.send("Job Tracker API"));
 
 // Global error handler (simple)
 app.use((err, req, res, next) => {
