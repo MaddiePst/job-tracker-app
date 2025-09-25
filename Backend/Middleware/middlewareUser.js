2701809cfe19c1b93c0022f14e88d10c115672ea
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-09-25 14:43:45 -0400

    Add files via upload

--- Backend/Middleware/middlewareUser.js
+++ Backend/Middleware/middlewareUser.js
@@ -5,15 +5,20 @@ export const SECRET = process.env.JWT_SECRET || "jdfhhak";
 
 export function authMiddleware(req, res, next) {
   const auth = req.headers.authorization;
+  // auth doesn't exists
   if (!auth || !auth.startsWith("Bearer ")) {
     return res
       .status(401)
       .json({ message: "Missing or invalid authorization header" });
   }
+  // Get the token
   const token = auth.split(" ")[1];
   try {
+    //verify the token structure
     const payload = jwt.verify(token, SECRET);
+    // Creates a user property on the req object and assigns it a small object with the authenticated user’s info.
     req.user = { id: payload.id, email: payload.email };
+    // Calls next() to tell Express: “this middleware is done
     return next();
   } catch (err) {
     console.error("auth verify error:", err && err.message);
