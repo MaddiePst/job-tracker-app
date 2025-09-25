363ad752116ff9073f9986ff0a982685439485e4
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-09-25 00:52:40 -0400

    Add files via upload

---
+++ Frontend/src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api", // proxied to your backend
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+export default api;
