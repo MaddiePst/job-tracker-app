d19db2ff9e7db3049a58b4a65eadadcc7599a209
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-09-25 14:44:44 -0400

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -3,17 +3,18 @@ import cors from "cors";
 import jobsRoutes from "./Routes/jobsRoutes.js";
 import usersRoutes from "./Routes/usersRoutes.js";
 
-// Middleware
 const app = express();
 app.use(express.json()); // parse JSON request bodies
 app.use(cors()); //allows requests from the frontend
 
+// Setting mini routers
 app.use("/jobs", jobsRoutes);
 app.use("/users", usersRoutes);
 
+// Initial route
 app.get("/", (req, res) => res.send("Job Tracker API"));
 
-// Global error handler (simple)
+// Global error handler
 app.use((err, req, res, next) => {
   console.error(err);
   res
