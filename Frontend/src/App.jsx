f85c5942d57f457b7a4578030cec3d229e8e6c4b
Madalina Pastiu <153855329+MaddiePst@users.noreply.github.com>
2025-09-27 12:37:51 -0400

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./Components/Header.jsx";
+import Login from "./Pages/LogIn.jsx";
+import Register from "./Pages/Register.jsx";
+import Dashboard from "./Pages/Dashboard.jsx";
+// import JobsList from "./Pages/JobsList.jsx";
+import JobForm from "./Pages/JobForm.jsx";
+import Profile from "./Pages/Profile.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import { isLoggedIn } from "./utils/auth.js";
+
+export default function App() {
+  return (
+    <>
+      {isLoggedIn() && <Header />} //I need to add more here
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Navigate to="/logIn" />} />
+          <Route path="/logIn" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Protected Routes */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            {/* <Route path="/jobs" element={<JobsList />} /> */}
+            <Route path="/jobs/new" element={<JobForm />} />
+            <Route path="/jobs/edit/:id" element={<JobForm />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
+          <Route path="*" element={<div>404 - Not Found</div>} />
+        </Routes>
+      </main>
+    </>
+  );
+}
